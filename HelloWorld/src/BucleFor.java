02427547c584f227adecf7dbcf331adfe55d605b
davdemete <davdemeter@gmail.com>
2023-02-27 13:26:28 +0100

    Proyecto JAVA en Eclipse

---
+++ HelloWorld/src/BucleFor.java
@@ -0,0 +1,18 @@
+
+public class BucleFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int edad3 = 1;
+		// comenzando con i=edad, mientras que i sea menor que 18 y con un incremento de i
+		de uno en uno
+		for (int i=edad3; i<18; i++) {
+		System.out.printf("%d", i).println();
+		}
+		System.out.printf("Programa finalizado --> edad: %d", edad3).println();
+	
+	for(int i=0; i<100; i--) {
+		System.out.println("Hola");
+		}
+		System.out.println("Adiós");
+
---
+++ HelloWorld/src/Ejercicio3.java
@@ -0,0 +1,21 @@
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class TablasMultiplicar {
+
+		    public static void main(String[] args) {
+		        for (int i = 1; i <= 9; i++) {
+		            System.out.println("Tabla de multiplicar del " + i + ":");
+		            for (int j = 0; j <= 10; j++) {
+		                System.out.println(i + " x " + j + " = " + (i*j));
+		            }
+		            System.out.println();
+		        }
+		    }
+		}
+
+	
+
+
