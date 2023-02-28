8587a854b732d369b3ed15500f85153e5b0df8b8
davdemete <davdemeter@gmail.com>
2023-02-28 09:18:33 +0100

    mas java

--- HelloWorld/src/ejercicio.java
+++ HelloWorld/src/ejercicio.java
@@ -3,7 +3,25 @@ public class ejercicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Escribe un numero:");
 
+		int numero = keyboard.nextInt();
+		keyboard.clone();
+
+		int i = 2;
+
+		while(numero % i != 0 && i < numero-1) {
+		        i++;
+		}
+
+		if (i == numero-1 || numero == 2) {
+		    System.out.println("El numero es primo");
+		}else if (numero <= 1) {
+		    System.out.println("Has puesto un 0, un 1 o un numero negativo");
+		}else {
+		    System.out.println("El numero no es primo");
+		}
 	}
 
 }
