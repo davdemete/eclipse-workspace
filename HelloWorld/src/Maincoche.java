1fca541c65cd0d9c1ccb1031d6d078aea80690e1
davdemete <davdemeter@gmail.com>
2023-02-28 13:53:37 +0100

    coche java

---
+++ HelloWorld/src/Maincoche.java
@@ -0,0 +1,16 @@
+
+public class Maincoche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Maincoche audi = new Maincoche();
+        //Metodo si es publico 
+        System.out.println(audi.getVelocidad)());
+        //No es publico el artibuto 
+//        audi.velocidad = 150.0f;
+        System.out.println(coche.getVelocidad)());
+        System.out.println(coche.convertirCvKw(200.0f));
+        
+	}
+
+}
