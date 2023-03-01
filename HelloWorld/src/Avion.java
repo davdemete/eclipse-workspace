841f6c236cd6076d6263a7d1b0991083d33d8a5d
davdemete <davdemeter@gmail.com>
2023-03-01 12:33:18 +0100

    proyectos

---
+++ HelloWorld/src/Avion.java
@@ -0,0 +1,7 @@
+
+public class Avion extens Vehiculo {
+	//Artibutos
+	private boolean trenAterrizage
+	//Metodos
+	public void volar() { }
+    public void aterrizage() { }
---
+++ HelloWorld/src/Vehiculo.java
@@ -0,0 +1,13 @@
+
+public class Vehiculo {
+    //Artibutos
+	protected float velocidad;
+	protected int asientos;
+	protected int orientacionRuedas;
+	//Metodos
+	public void parar() {}
+	public void acelerar() {}
+	public void desacelerar() {}
+    public void girarIzquierda() {}
+    public void girarDerecha() {}
+}
